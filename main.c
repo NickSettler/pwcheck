d66a6472eff945679902253241e392c182c5e0b0
NickSettler <moiseevnikita14@gmail.com>
2021-10-27 16:50:23 +0200

    refactoring

--- main.c
+++ main.c
@@ -56,7 +56,8 @@ int char_code(char c) {
  * @return if character is non-printing symbol
  */
 bool is_system_symbol(char c) {
-    return char_code(c) >= MIN_SYSTEM_SYMBOL_CODE && char_code(c) <= MAX_SYSTEM_SYMBOL_CODE;
+    return char_code(c) >= MIN_SYSTEM_SYMBOL_CODE &&
+           char_code(c) <= MAX_SYSTEM_SYMBOL_CODE;
 }
 
 /**
@@ -65,7 +66,8 @@ bool is_system_symbol(char c) {
  * @return if character is lower letter
  */
 bool is_lower_letter(char c) {
-    return char_code(c) >= MIN_LOWER_LETTER_CODE && char_code(c) <= MAX_LOWER_LETTER_CODE;
+    return char_code(c) >= MIN_LOWER_LETTER_CODE &&
+           char_code(c) <= MAX_LOWER_LETTER_CODE;
 }
 
 /**
@@ -74,7 +76,8 @@ bool is_lower_letter(char c) {
  * @return if character is upper letter
  */
 bool is_upper_letter(char c) {
-    return char_code(c) >= MIN_UPPER_LETTER_CODE && char_code(c) <= MAX_UPPER_LETTER_CODE;
+    return char_code(c) >= MIN_UPPER_LETTER_CODE &&
+           char_code(c) <= MAX_UPPER_LETTER_CODE;
 }
 
 /**
@@ -83,7 +86,8 @@ bool is_upper_letter(char c) {
  * @return if character is number
  */
 bool is_number(char c) {
-    return char_code(c) >= MIN_NUMBER_CODE && char_code(c) <= MAX_NUMBER_CODE;
+    return char_code(c) >= MIN_NUMBER_CODE &&
+           char_code(c) <= MAX_NUMBER_CODE;
 }
 
 /**
@@ -92,8 +96,11 @@ bool is_number(char c) {
  * @return if character is special symbol
  */
 bool is_specific_symbol(char c) {
-    return char_code(c) >= MIN_SPEC_SYMBOL_CODE && char_code(c) <= MAX_SPEC_SYMBOL_CODE && !is_lower_letter(c) &&
-           !is_upper_letter(c) && !is_number(c);
+    return char_code(c) >= MIN_SPEC_SYMBOL_CODE &&
+           char_code(c) <= MAX_SPEC_SYMBOL_CODE &&
+           !is_lower_letter(c) &&
+           !is_upper_letter(c) &&
+           !is_number(c);
 }
 
 /**
@@ -114,6 +121,7 @@ size_t str_length(char *str) {
  */
 size_t str_length_only_chars(char *str) {
     int size = 0;
+
     for (int i = 0; str[i]; i++)
         if (!is_system_symbol(str[i]))
             size++;
@@ -130,6 +138,7 @@ size_t str_length_only_chars(char *str) {
  */
 void substring(const char *str, char *substring, int start, int length) {
     int c = 0;
+
     while (c < length) {
         substring[c] = str[start + c];
         c++;
@@ -211,8 +220,14 @@ bool second_rule_check(char *str, int level) {
     if (!first_rule_succeeded) return false;
 
     if (level > 4) level = 4;
+
     bool conditions[level];
-    func_ptr rule_functions[4] = {is_lower_letter, is_upper_letter, is_number, is_specific_symbol};
+    func_ptr rule_functions[4] = {
+            is_lower_letter,
+            is_upper_letter,
+            is_number,
+            is_specific_symbol
+    };
 
     for (int i = 0; i < level; i++) {
         conditions[i] = false;
@@ -289,7 +304,10 @@ bool fourth_rule_check(char *str, int level) {
     bool second_rule_succeeded = second_rule_check(str, level);
     bool third_rule_succeeded = third_rule_check(str, level);
 
-    if (!first_rule_succeeded || !second_rule_succeeded || !third_rule_succeeded) return false;
+    if (!first_rule_succeeded ||
+        !second_rule_succeeded ||
+        !third_rule_succeeded)
+        return false;
 
     int first_repeat_index_a = -1;
     int first_repeat_index_b = -1;
@@ -306,7 +324,8 @@ bool fourth_rule_check(char *str, int level) {
                 substring(str, substring_b, j, level);
 
                 if (is_equal(substring_a, substring_b)) {
-                    if (first_repeat_index_a == -1 && first_repeat_index_b == -1) {
+                    if (first_repeat_index_a == -1 &&
+                        first_repeat_index_b == -1) {
                         first_repeat_index_a = i;
                         first_repeat_index_b = j;
                     } else {
@@ -352,10 +371,15 @@ void update_chars_table(int *table, const char *str) {
  */
 void print_stats(stats statistics) {
     printf(
+            "\n"
             "Statistika:\n"
             "Ruznych znaku: %d\n"
             "Minimalni delka: %d\n"
-            "Prumerna delka: %.1f", statistics.n_chars, statistics.min, statistics.avg);
+            "Prumerna delka: %.1f",
+            statistics.n_chars,
+            statistics.min,
+            statistics.avg
+    );
 }
 
 /**
@@ -375,7 +399,11 @@ int main(int argc, char *argv[]) {
             {"stats", no_argument, 0, 0}
     };
 
-    while ((opt = getopt_long(argc, argv, "f:l:", long_options, &option_index)) != -1) {
+    while ((opt = getopt_long(argc,
+                              argv,
+                              "f:l:",
+                              long_options,
+                              &option_index)) != -1) {
         switch (opt) {
             case 0:
                 need_stats = true;
@@ -391,8 +419,6 @@ int main(int argc, char *argv[]) {
         }
     }
 
-//    printf("Current level: %d\nCurrent extra param: %d\nStatistics needed: %s", level, param, need_stats ? "true" : "false");
-
     char input_passwords[100];
 
     int chars_table[128] = {0};
@@ -400,11 +426,16 @@ int main(int argc, char *argv[]) {
     double passwords_count = 0;
     double passwords_chars_count = 0;
 
-    rule_func_ptr check_functions[4] = {&first_rule_check, &second_rule_check, &third_rule_check, &fourth_rule_check};
+    rule_func_ptr check_functions[4] = {
+            &first_rule_check,
+            &second_rule_check,
+            &third_rule_check,
+            &fourth_rule_check
+    };
 
     while (fgets(input_passwords, 100, stdin)) {
         if (check_functions[level - 1](input_passwords, param))
-            printf("%s\n", input_passwords);
+            printf("%s", input_passwords);
 
 
         update_chars_table(chars_table, input_passwords);
@@ -424,11 +455,6 @@ int main(int argc, char *argv[]) {
             statistics.avg = avg;
             print_stats(statistics);
         }
-
-
-//        WORKS!
-
-//        WORKS!
     }
 
     return 0;
